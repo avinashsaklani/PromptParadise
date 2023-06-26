2cd3c81b2c6496e039e46ebb4e70a24b903ae692
avinashsaklani <avinashsaklani7@gmail.com>
2023-06-27 00:28:43 +0530

    Adding Create Prompt Functionality

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,19 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+export const POST = async (req) => {
+    const { userID, prompt, tag } = await req.json();
+
+    try {
+        await connectToDB();
+        const newPrompt = new Prompt({
+            creator: userID,
+            prompt,
+            tag
+        })
+        await newPrompt.save();
+
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
+    } catch (error) {
+        return new Response("Failed to create a new Prompt", { status: 500 })
+    }
+}
